73941ef4ed0b1170b641516a0da4236cc435c09d
annse <annmiro@gmail.com>
2022-09-27 23:31:04 +0300

    dz5 remake

--- src/lesson4/Calculator.java
+++ src/lesson4/Calculator.java
@@ -3,46 +3,30 @@
  * @data 25.09.22
  */
 package lesson4;
-
 import java.util.Scanner;
-
 public class Calculator {
-    static boolean askExit = true;
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double firstNumber = getNumber();
-        double secondNumber = getNumber();
-        String operator = getOperator();
-        calculate(firstNumber, secondNumber, operator);
-        do {
-            askExit = askExit();
-        }while(askExit);
+
+        boolean calculator = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while(calculator){
+            System.out.println("Enter a first number");
+            double firstNumber = scanner.nextInt();
+            System.out.println("Enter a second number");
+            double secondNumber = scanner.nextInt();
+            System.out.println("Enter an operator (+ - * /)");
+            String operator = scanner.next();
+
+            calculate(firstNumber, secondNumber, operator);
+
+            System.out.println("Do you wont to exit? print 1 if yes");
+            int exit = scanner.nextInt();
+            calculator = exit == 1? false : true;
+        }
+
         scanner.close();
-    }
-    public static double getNumber(){
-        System.out.println("Enter a number");
-        double number;
-        if(scanner.hasNextDouble()){
-            number = scanner.nextInt();
-        } else {
-            System.out.println("Error. Try again.");
-            scanner.next();
-            number = getNumber();
-        }
-        return number;
-    }
-    public static String getOperator(){
-        System.out.println("Enter an operator (+ - * /)");
-        String operator;
-        if(scanner.hasNext()){
-            operator = scanner.next();
-        } else {
-            System.out.println("Error. Try again.");
-            scanner.next();
-            operator = getOperator();
-        }
-        return operator;
     }
     public static void calculate(double num, double num2, String operator){
 
@@ -62,13 +46,5 @@ public class Calculator {
             default:
                 System.err.println("Invalid action");
         }
-    }
-
-    public static boolean askExit(){
-        String exit;
-        System.out.println("Do you wont to exit? print exit if yes");
-        Scanner scanner = new Scanner(System.in);
-        exit = scanner.nextLine();
-        return exit.equals("exit")? false : true;
     }
 }
--- src/lesson4/StringModification.java
+++ src/lesson4/StringModification.java
@@ -3,27 +3,12 @@
  * @data 25.09.22
  */
 package lesson4;
-
 public class StringModification {
-    public static String text = "ZюукяДZіботZ,ежобZощZяZенZ!ьлаксом";
-    public static String splitFrom = "Z";
-    public static String splitTo = " ";
-
     public static void main(String[] args) {
+        String text = "ZюукяДZіботZ,ежобZощZяZенZ!ьлаксом";
 
-        String[] textAr = splitString(text, splitFrom);
-        String reversedText = reverseText(textAr, splitTo);
-        System.out.println(reversedText);
-    }
-    public static String[] splitString(String text, String splitFrom) {
-        String[] splittedArText = text.split(splitFrom);
-        return splittedArText;
-    }
-    public static String reverseText(String[] text, String splitTo) {
-        String reversedText = "";
-        for(String word : text){
-            reversedText += new StringBuilder(word).reverse() + splitTo;
+        for(String word : text.split("Z")){
+            System.out.print(new StringBuilder(word).reverse() + " ");
         }
-        return reversedText.trim();
     }
 }
